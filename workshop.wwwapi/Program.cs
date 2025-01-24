23ace6dbe422bf53312b5771c23b4223c2cabbbe
Jone <64496635+JoneTheBuilder@users.noreply.github.com>
2025-01-24 09:38:50 +0100

    Testing if .gitignore ignores appsettings.json and appsettings.Development.json

--- workshop.wwwapi/Program.cs
+++ workshop.wwwapi/Program.cs
@@ -4,15 +4,12 @@ using workshop.wwwapi.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DatabaseContext>();
 builder.Services.AddScoped<IRepository,Repository>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
